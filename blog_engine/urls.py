5ba1475c6df8d9f342e4d0a2847fee005b0e842b
krystian <krystian.wlr@gmail.com>
2018-06-12 16:02:59 +0200

    Fully working basic version of the blog

--- blog_engine/urls.py
+++ blog_engine/urls.py
@@ -8,5 +8,14 @@ app_name = 'blog_engine'
 
 urlpatterns = [
     #Post list
-url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
+
+    #Single post
+    url(r'^post/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
+
+    #New post
+    url(r'^post/new/$', views.post_new, name='post_new'),
+
+    #Post edit
+    url(r'^post/(?P<post_id>\d+)/edit/$', views.post_edit, name='post_edit')
 ]
--- blog_engine/views.py
+++ blog_engine/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.utils import timezone
+from django.urls import reverse
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog_engine/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog_engine/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return HttpResponseRedirect(reverse('blog_engine:post_detail', args=[post.id]))
+    else:
+        form = PostForm()
+    return render(request, 'blog_engine/post_edit.html', {'form': form})
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return HttpResponseRedirect(reverse('blog_engine:post_detail', args=[post.id]))
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog_engine/post_edit.html', {'form': form})
